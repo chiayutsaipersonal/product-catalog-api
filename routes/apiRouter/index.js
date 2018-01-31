3362c954e31dd138d5690ca8f2ce3eae84603393
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2018-01-31 22:23:58 +0800

    api routing
    modulizing

--- routes/apiRouter/index.js
+++ routes/apiRouter/index.js
@@ -1,25 +1,23 @@
 const express = require('express')
 
 // route handlers
-const authentication = require('../controllers/authentication')()
-const responseHandlers = require('../middlewares/responseHandlers')
-const contactApiHandlers = require('./api/contacts')
-const testHandlers = require('./api/test')
+const authentication = require('../../controllers/authentication')()
+const responseHandlers = require('../../middlewares/responseHandlers')
 
 const apiRouterV1 = express.Router()
 
-module.exports = { init }
+module.exports = {
+  init,
+  apiRouterV1,
+}
 
 function init (app) {
   /* API routes specific global middlewares */
   app.use('/api/v1', apiRouterV1)
   apiRouterV1.use(authentication.initialize())
+
   /* API routes */
-  apiRouterV1.param('contactId', contactApiHandlers.autoFindTarget)
-  apiRouterV1.route('/login').post(...contactApiHandlers.login)
-  apiRouterV1.route('/test/plain').get(...testHandlers.plain)
-  apiRouterV1.route('/test/contacts/:contactId').get(...testHandlers.specificUser)
-  apiRouterV1.route('/test/adminsOnly').get(...testHandlers.adminsOnly)
+  require('./contacts') // contact related routes
 
   /* post routing middlewares */
   apiRouterV1.use(responseHandlers.file)
